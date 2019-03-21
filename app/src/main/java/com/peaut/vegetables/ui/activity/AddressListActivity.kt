ab2fb55fef99d6d3eaf4587383a3b39db0f60c8e
admin <1079793182@qq.com>
2019-03-21 10:16:49 +0800

    布局优化和删除默认地址数据

--- app/src/main/java/com/peaut/vegetables/ui/activity/AddressListActivity.kt
+++ app/src/main/java/com/peaut/vegetables/ui/activity/AddressListActivity.kt
@@ -36,12 +36,6 @@ class AddressListActivity : BaseActivity(), OnAddressItemListener {
     }
 
     override fun initData(savedInstanceState: Bundle?) {
-        val data = arrayListOf<Address>()
-        data.add(Address("王冲冲","15279832376","深圳市宝安区西乡街道西乡大道华盛辉大厦C栋大门岗亭旁丰巢柜",0,true))
-        data.add(Address("王冲冲","15279832376","深圳市宝安区西乡街道西乡大道华盛辉大厦C栋大门岗亭旁丰巢柜",0,false))
-        data.add(Address("王冲冲","15279832376","深圳市宝安区西乡街道西乡大道华盛辉大厦C栋大门岗亭旁丰巢柜",0,false))
-        data.add(Address("王冲冲","15279832376","深圳市宝安区西乡街道西乡大道华盛辉大厦C栋大门岗亭旁丰巢柜",0,false))
-        adapter.addAll(data)
         addressViewModel.queryAddress()
     }
 
